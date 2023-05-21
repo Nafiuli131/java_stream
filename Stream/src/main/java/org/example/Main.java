ed9ab86ff8f95ffe9be65e383e62f69954ea80f3
Nafiul-Islam-Nayeem <nafiul.islam@digigate360.com>
2023-05-21 16:53:02 +0600

    basic stream

--- Stream/src/main/java/org/example/Main.java
+++ Stream/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> numbers = Arrays.asList(1, 22, 3, 41, 59, 16, 97);
+        Stream<Integer> streamInteger = numbers.stream();
+
+        List<String> strings = Arrays.asList("Nafiul", "Islam", "Nayeem", "Nafi", "Red", "Purple");
+        Stream<String> stringStream = strings.stream();
+
+        //add 5 in every numbers here
+        List<Integer> integers = streamInteger.map(integer -> integer + 10).collect(Collectors.toList());
+        System.out.println(integers);
+
+        List<String> stringList = stringStream.filter(value -> value.length() > 5).toList();
+        System.out.println(stringList);
+
+        int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
+
+
     }
 }
