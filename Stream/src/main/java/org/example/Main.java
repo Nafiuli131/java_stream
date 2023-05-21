a6b09d7ddd1bedcbe74b8512e37cb0af82ed15d5
Nafiul-Islam-Nayeem <nafiul.islam@digigate360.com>
2023-05-21 18:01:50 +0600

    some problems

--- Stream/src/main/java/org/example/Main.java
+++ Stream/src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         List<Integer> numbers = Arrays.asList(1, 22, 3, 41, 59, 16, 97);
         Stream<Integer> streamInteger = numbers.stream();
 
-        List<String> strings = Arrays.asList("Nafiul", "Islam", "Nayeem", "Nafi", "Red", "Purple");
+        List<String> strings = Arrays.asList("Nafiul", "Islam", "Nayeem", "Nafiul_Islam_Nayeem", "Nafi", "Red", "Purple");
         Stream<String> stringStream = strings.stream();
 
         //add 5 in every numbers here
@@ -23,6 +23,14 @@ public class Main {
 
         int sum = numbers.stream().reduce(0, (a, b) -> a + b);
         System.out.println(sum);
+
+        // Given a list of integers, find the sum of all even numbers.
+        Optional<Integer> askSum = numbers.stream().filter(integer -> (integer % 2) == 0).reduce((a, b) -> a + b);
+        System.out.println(askSum.get());
+
+        //Given a list of names, find the length of the longest name.
+        String longestName = String.valueOf(stringList.stream().reduce((a, b) -> a.length() > b.length() ? a : b));
+        System.out.println(longestName);
 
 
     }
